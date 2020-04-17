d4444f70c3458fe75f1e915907623a918f35c923
Ben Black <weepingwillowben@gmail.com>
2020-04-17 11:34:38 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SuperSuit",
+    version="0.1.10",
+    author="PettingZoo Team",
+    author_email="justinkterry@gmail.com",
+    description="Wrappers for Gym and PettingZoo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PettingZoo-Team/SuperSuit",
+    keywords=["Reinforcement Learning", "gym"],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+    data_files = [("", ["LICENSE.txt"])],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
