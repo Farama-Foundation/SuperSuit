4b63206111785b0d95086a0d12ab90888c35f1c3
Ben Black <weepingwillowben@gmail.com>
2020-10-14 14:13:28 -0400

    updated pettingzoo integration tests

--- test/pettingzoo_api_test.py
+++ test/pettingzoo_api_test.py
@@ -1,5 +1,5 @@
 from pettingzoo.tests import api_test, seed_test, error_tests, parallel_test
-from pettingzoo.mpe import simple_push_v0, simple_world_comm_v0
+from pettingzoo.mpe import simple_push_v1, simple_world_comm_v1
 # from pettingzoo.sisl import multiwalker
 
 from supersuit import frame_stack_v1, pad_action_space_v0, frame_skip_v0, sticky_actions_v0
@@ -7,13 +7,13 @@ import numpy as np
 
 
 def test_pettinzoo_frame_stack():
-    _env = simple_push_v0.env()
+    _env = simple_push_v1.env()
     wrapped_env = frame_stack_v1(_env)
     api_test.api_test(wrapped_env)
 
 
 def test_pettinzoo_frame_skip():
-    env = simple_push_v0.raw_env()
+    env = simple_push_v1.raw_env()
     env = frame_skip_v0(env, 3)
     env.reset()
     for x in range(10):
@@ -23,13 +23,13 @@ def test_pettinzoo_frame_skip():
 
 
 def test_pettinzoo_pad_actino_space():
-    _env = simple_world_comm_v0.env()
+    _env = simple_world_comm_v1.env()
     wrapped_env = pad_action_space_v0(_env)
     api_test.api_test(wrapped_env)
-    seed_test.seed_test(lambda: sticky_actions_v0(simple_world_comm_v0.env(), 0.5))
+    seed_test.seed_test(lambda: sticky_actions_v0(simple_world_comm_v1.env(), 0.5))
 
 
 def test_pettingzoo_parallel_env():
-    _env = simple_world_comm_v0.parallel_env()
+    _env = simple_world_comm_v1.parallel_env()
     wrapped_env = pad_action_space_v0(_env)
     parallel_test.parallel_play_test(wrapped_env)
