e92d943e500e960bb2b7514f8bee3062c03c2dce
Ben Black <weepingwillowben@gmail.com>
2020-11-22 12:15:49 -0500

    fixed parallel frame skip wrapper

--- supersuit/parallel_wrappers.py
+++ supersuit/parallel_wrappers.py
@@ -95,13 +95,20 @@ class frame_skip(ParallelWraper):
     def step(self, action):
         low, high = self.num_frames
         num_skips = int(self.np_random.randint(low, high + 1))
-        total_reward = {agent: 0.0 for agent in self.agents}
+        next_agents = self.env.agents[:]
+        total_reward = {agent: 0.0 for agent in self.env.agents}
+        total_dones = {}
+        total_infos = {}
+        total_obs = {}
 
         for x in range(num_skips):
             obs, rews, done, info = super().step(action)
             for agent, rew in rews.items():
                 total_reward[agent] += rew
+                total_dones[agent] = done[agent]
+                total_infos[agent] = info[agent]
+                total_obs[agent] = obs[agent]
             if all(done.values()):
                 break
-
-        return obs, total_reward, done, info
+        self.agents = next_agents
+        return total_obs, total_reward, total_dones, total_infos
