cd9fcaf7e23d18cd5a94506a75fc260c1fd5ab3a
Costa Huang <costa.huang@outlook.com>
2021-03-01 11:06:57 -0500

    inherit using `VecEnvWrapper`

--- supersuit/vector/sb3_vector_wrapper.py
+++ supersuit/vector/sb3_vector_wrapper.py
@@ -1,8 +1,7 @@
-from stable_baselines3.common.vec_env.base_vec_env import VecEnv
+from stable_baselines3.common.vec_env import VecEnvWrapper
 import warnings
 
-
-class SB3VecEnvWrapper(VecEnv):
+class SB3VecEnvWrapper(VecEnvWrapper):
     def __init__(self, venv):
         self.venv = venv
         self.num_envs = venv.num_envs
@@ -12,29 +11,5 @@ class SB3VecEnvWrapper(VecEnv):
     def reset(self):
         return self.venv.reset()
 
-    def step_async(self, actions):
-        self.venv.step_async(actions)
-
     def step_wait(self):
         return self.venv.step_wait()
-
-    def step(self, actions):
-        return self.venv.step(actions)
-
-    def close(self):
-        del self.venv
-
-    def seed(self, seed=None):
-        self.venv.seed(seed)
-
-    def get_attr(self, attr_name, indices=None):
-        raise NotImplementedError()
-
-    def set_attr(self, attr_name, value, indices=None):
-        raise NotImplementedError()
-
-    def env_method(self, method_name, *method_args, indices=None, **method_kwargs):
-        raise NotImplementedError()
-
-    def env_is_wrapped(self, wrapper_class, indices):
-        raise NotImplementedError()
