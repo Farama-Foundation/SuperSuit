3e0855ec9f492f32e1899871143a32675c41a0f4
Ben Black <weepingwillowben@gmail.com>
2021-03-11 13:23:04 -0500

    changed test to pistonball

--- test/test_vector/test_render.py
+++ test/test_vector/test_render.py
@@ -1,10 +1,17 @@
 from supersuit import gym_vec_env_v0, concat_vec_envs_v0
 import gym
 import numpy as np
+from pettingzoo.butterfly import pistonball_v4
+from supersuit import concat_vec_envs_v0, pettingzoo_env_to_vec_env_v0
 
+
+def make_env():
+    env = pistonball_v4.parallel_env()
+    env = pettingzoo_env_to_vec_env_v0(env)
+    return env
 
 def test_vector_render_multiproc():
-    env = gym.make("SpaceInvaders-v4")
+    env = make_env()
     num_envs = 3
     venv = concat_vec_envs_v0(env, num_envs, num_cpus=num_envs, base_class='stable_baselines3')
     venv.reset()
@@ -14,7 +21,7 @@ def test_vector_render_multiproc():
 
 
 def test_vector_render_multiproc_human():
-    env = gym.make("SpaceInvaders-v4")
+    env = make_env()
     num_envs = 3
     venv = concat_vec_envs_v0(env, num_envs, num_cpus=num_envs, base_class='stable_baselines3')
     venv.reset()
@@ -23,16 +30,17 @@ def test_vector_render_multiproc_human():
 
 
 def test_vector_render_single_proc():
-    env = gym.make("SpaceInvaders-v4")
+    env = make_env()
     num_envs = 1
     venv = concat_vec_envs_v0(env, num_envs, num_cpus=num_envs)
     venv.reset()
     venv.render(mode="rgb_array")
+    assert len(arr.shape) == 3 and arr.shape[2] == 3
     venv.close()
 
 
 def test_vector_render_single_proc_human():
-    env = gym.make("SpaceInvaders-v4")
+    env = make_env()
     num_envs = 1
     venv = concat_vec_envs_v0(env, num_envs, num_cpus=num_envs)
     venv.reset()
