686df68e4e3f857d90e003986dc32e8b667be93f
Ben Black <weepingwillowben@gmail.com>
2021-04-19 22:21:32 -0400

    added support for env_is_wrapped

--- supersuit/vector/sb3_vector_wrapper.py
+++ supersuit/vector/sb3_vector_wrapper.py
@@ -15,6 +15,6 @@ class SB3VecEnvWrapper(VecEnvWrapper):
     def step_wait(self):
         return self.venv.step_wait()
 
-    def env_is_wrapped(self, wrapper_class, indices):
-        n_returns = len(indices) if indices is not None else self.num_envs
-        return [False] * n_returns
+    def env_is_wrapped(self, wrapper_class, indices=None):
+        # ignores indicies
+        return self.venv.env_is_wrapped(wrapper_class)
