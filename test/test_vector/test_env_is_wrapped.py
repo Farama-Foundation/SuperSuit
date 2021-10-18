996f4d6a798c5ae575035bdf0f280f2ac3317ce7
Ben Black <weepingwillowben@gmail.com>
2021-10-17 19:07:42 -0600

    worked around weird CI issue

--- test/test_vector/test_env_is_wrapped.py
+++ test/test_vector/test_env_is_wrapped.py
@@ -5,7 +5,7 @@ from pettingzoo.mpe import simple_spread_v2
 
 
 def test_env_is_wrapped_true():
-    env = gym.make("Pendulum-v1")
+    env = gym.make("MountainCarContinuous-v0")
     env = frame_skip_gym(env, 4)
     num_envs = 3
     venv1 = concat_vec_envs_v0(env, num_envs)
@@ -13,14 +13,14 @@ def test_env_is_wrapped_true():
 
 
 def test_env_is_wrapped_false():
-    env = gym.make("Pendulum-v1")
+    env = gym.make("MountainCarContinuous-v0")
     num_envs = 3
     venv1 = concat_vec_envs_v0(env, num_envs)
     assert venv1.env_is_wrapped(frame_skip_gym) == [False] * 3
 
 
 def test_env_is_wrapped_cpu():
-    env = gym.make("Pendulum-v1")
+    env = gym.make("MountainCarContinuous-v0")
     env = frame_skip_gym(env, 4)
     num_envs = 3
     venv1 = concat_vec_envs_v0(env, num_envs, num_cpus=2)
