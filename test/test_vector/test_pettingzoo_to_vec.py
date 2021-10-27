a31a16489188331766f8e30e701daaef3ec299c2
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2021-10-27 18:02:19 +0200

    Update test_pettingzoo_to_vec.py

--- test/test_vector/test_pettingzoo_to_vec.py
+++ test/test_vector/test_pettingzoo_to_vec.py
@@ -1,6 +1,8 @@
+import copy
+
 from pettingzoo.mpe import simple_spread_v2, simple_world_comm_v2
 from pettingzoo.butterfly import knights_archers_zombies_v7
-from supersuit import pettingzoo_env_to_vec_env_v0, black_death_v2
+from supersuit import pettingzoo_env_to_vec_env_v0, black_death_v2, concat_vec_envs_v0
 import pytest
 
 
@@ -10,17 +12,49 @@ def test_good_env():
     env = pettingzoo_env_to_vec_env_v0(env)
     assert env.num_envs == max_num_agents
 
-    env.reset()
+    obss = env.reset()
     for i in range(55):
         actions = [env.action_space.sample() for i in range(env.num_envs)]
-        obss, rews, dones, infos = env.step(actions)
-        assert len(obss) == max_num_agents
+
+        # Check we're not passing a thing that gets mutated
+        keep_obs = copy.deepcopy(obss)
+        new_obss, rews, dones, infos = env.step(actions)
+
+        assert hash(str(keep_obs)) == hash(str(obss))
+        assert len(new_obss) == max_num_agents
         assert len(rews) == max_num_agents
         assert len(dones) == max_num_agents
         assert len(infos) == max_num_agents
         # no agent death, only env death
         if any(dones):
             assert all(dones)
+        obss = new_obss
+
+def test_good_vecenv():
+    num_envs = 2
+    env = simple_spread_v2.parallel_env()
+    max_num_agents = len(env.possible_agents) * num_envs
+    env = pettingzoo_env_to_vec_env_v0(env)
+    env = concat_vec_envs_v0(env, num_envs)
+
+
+    obss = env.reset()
+    for i in range(55):
+        actions = [env.action_space.sample() for i in range(env.num_envs)]
+
+        # Check we're not passing a thing that gets mutated
+        keep_obs = copy.deepcopy(obss)
+        new_obss, rews, dones, infos = env.step(actions)
+
+        assert hash(str(keep_obs)) == hash(str(obss))
+        assert len(new_obss) == max_num_agents
+        assert len(rews) == max_num_agents
+        assert len(dones) == max_num_agents
+        assert len(infos) == max_num_agents
+        # no agent death, only env death
+        if any(dones):
+            assert all(dones)
+        obss = new_obss
 
 
 def test_bad_action_spaces_env():
