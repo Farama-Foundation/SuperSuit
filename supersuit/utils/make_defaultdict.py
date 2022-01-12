74a5f7de10e09c92bb7dba6d6558edda9f126552
Sathvik Ravi <sathvikrav@gmail.com>
2022-01-12 12:19:22 -0500

    Added more descriptive error message for StopIteration exception
    Added more descriptive error message for StopIteration exception in make_defaultdict.py file as per Issue #600.

--- supersuit/utils/make_defaultdict.py
+++ supersuit/utils/make_defaultdict.py
@@ -7,5 +7,6 @@ def make_defaultdict(d):
         for k, v in d.items():
             dd[k] = v
         return dd
-    except StopIteration:
+    except StopIteration as e:
+        print('{}: All data has been iterated over.'.format(type(e).__name__))
         return {}
