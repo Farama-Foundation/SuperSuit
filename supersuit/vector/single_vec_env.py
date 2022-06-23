1bfc3660fee077148b0fbce57f68cb8e6382c059
Jet <38184875+jjshoots@users.noreply.github.com>
2022-06-23 09:21:54 +0100

    Remove fix for np random

--- supersuit/vector/single_vec_env.py
+++ supersuit/vector/single_vec_env.py
@@ -7,7 +7,6 @@ class SingleVecEnv:
     def __init__(self, gym_env_fns, *args):
         assert len(gym_env_fns) == 1
         self.gym_env = gym_env_fns[0]()
-        self.gym_env.unwrapped.np_random, _ = np_random()
         self.observation_space = self.gym_env.observation_space
         self.action_space = self.gym_env.action_space
         self.num_envs = 1
