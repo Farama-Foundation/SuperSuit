ece159089711f56a9adcf7fa8453eeb9ea67d7fc
will <will2346@live.co.uk>
2022-09-17 13:02:46 +0100

    vectorise_aec_env_v0() running without bugs with 1 environment

--- sanity_checks/base_aec_tests.py
+++ sanity_checks/base_aec_tests.py
@@ -9,11 +9,10 @@ env = pistonball_v6.env()
 env = vectorize_aec_env_v0(env, 1)
 
 env.reset()
-for agent in env.agent_iter():
-    obs, reward, trunc, term, info = env.last()
-    if trunc or term:
+for agent in env.agent_iter(10):
+    obs, cum_reward, term, trunc, env_terms, env_truncs, passes, info = env.last()
+    if (trunc & term).all():
         act = None
     else:
         act = env.action_space(agent).sample()
     env.step(act)
-    env.render()
