d305ef8fa93490ca6f1400349ee3b419e760b2eb
will <will2346@live.co.uk>
2022-09-17 20:14:13 +0100

    naive test both wrapper options

--- sanity_checks/agent_indication_tests.py
+++ sanity_checks/agent_indication_tests.py
@@ -15,3 +15,15 @@ for agent in env.agent_iter(1000000):
     if (np.array(term) & np.array(trunc)).all():
         env.reset()
     env.step(act)
+
+
+env = pistonball_v6.env()
+env = agent_indicator_v0(env, type_only=True)
+
+env.reset()
+for agent in env.agent_iter(1000000):
+    obs, rew, term, trunc, info = env.last()
+    act = None if (term or trunc) else env.action_space(agent).sample()
+    if (np.array(term) & np.array(trunc)).all():
+        env.reset()
+    env.step(act)
