0df639e7ea174510bb31414ec4671f744aac0940
will <will2346@live.co.uk>
2022-09-18 12:12:07 +0100

    black death sanity tests runs without bugs

---
+++ sanity_checks/black_death_tests.py
@@ -0,0 +1,19 @@
+"""
+Just to ensure wrappers run
+"""
+import numpy as np
+from pettingzoo.butterfly import pistonball_v6
+from supersuit.multiagent_wrappers import black_death_v3
+
+env = pistonball_v6.env()
+env = black_death_v3(env)
+
+env.reset()
+for agent in env.agent_iter(1000000):
+    obs, rew, term, trunc, info = env.last()
+    act = None if (term or trunc) else env.action_space(agent).sample()
+    if (np.array(term) & np.array(trunc)).all():
+        env.reset()
+        break
+    print(__name__, term, trunc, act)
+    env.step(act)
