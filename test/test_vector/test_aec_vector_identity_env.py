b8b31dcdad75a691f15ecbb92ea71abcda4bd53d
will <will2346@live.co.uk>
2022-09-19 18:07:19 +0100

    trunc update

--- test/test_vector/test_aec_vector_identity_env.py
+++ test/test_vector/test_aec_vector_identity_env.py
@@ -28,12 +28,14 @@ def test_identical():
     for agent in env1.agent_iter(200000):
         assert env1.agent_selection == env2.agent_selection
         agent = env1.agent_selection
-        obs1, rew1, agent_done1, env_done1, agent_passes1, infos1 = env1.last()
-        obs2, rew2, agent_done2, env_done2, agent_passes2, infos2 = env2.last()
+        obs1, rew1, agent_term1, agent_trunc1, env_term1, env_trunc1, agent_passes1, infos1 = env1.last()
+        obs2, rew2, agent_term2, agent_trunc2, env_term2, env_trunc2, agent_passes2, infos2 = env2.last()
         assert np.all(np.equal(obs1, obs2))
-        assert np.all(np.equal(agent_done1, agent_done2))
+        assert np.all(np.equal(agent_term1, agent_term2))
+        assert np.all(np.equal(agent_trunc1, agent_trunc2))
         assert np.all(np.equal(agent_passes1, agent_passes2))
-        assert np.all(np.equal(env_done1, env_done2))
+        assert np.all(np.equal(env_term1, env_term2))
+        assert np.all(np.equal(env_trunc1, env_trunc2))
         assert np.all(np.equal(obs1, obs2))
         assert all(
             np.all(np.equal(r1, r2))
@@ -48,8 +50,8 @@ def test_identical():
             # if agent_passes[j]:
             #     print("pass")
             if rew1[j] != 0:
-                print(j, agent, rew1, agent_done1[j])
-            if env_done1[j]:
+                print(j, agent, rew1, agent_term1[j], agent_trunc1[j])
+            if env_term1[j] or env_trunc1[j]:
                 print(j, "done")
                 envs_done += 1
                 if envs_done == n_envs + 1:
