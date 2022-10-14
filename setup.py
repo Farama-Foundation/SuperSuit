3130cd4551dfb72c1dcb804bb3b120e75f18f310
Jet <taijunjet@hotmail.com>
2022-10-14 13:32:56 +0100

    merge conflicts

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-import setuptools
+"""Setups up the SuperSuit module."""
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import find_packages, setup
+
+
+def get_description():
+    """Gets the description from the readme."""
+    with open("README.md") as fh:
+        long_description = ""
+        header_count = 0
+        for line in fh:
+            if line.startswith("##"):
+                header_count += 1
+            if header_count < 2:
+                long_description += line
+            else:
+                break
+    return header_count, long_description
 
 
 def get_version():
+    """Gets the pettingzoo version."""
     path = "supersuit/__init__.py"
     with open(path) as file:
         lines = file.readlines()
@@ -14,8 +29,35 @@ def get_version():
             return line.strip().split()[-1].strip().strip('"')
     raise RuntimeError("bad version data in __init__.py")
 
+version = get_version()
+header_count, long_description = get_description()
 
-setuptools.setup(
+setup(
+    name="SuperSuit",
+    version=version,
+    author="Farama Foundation",
+    author_email="contact@farama.org",
+    description="Wrappers for Gymnasium and PettingZoo",
+    url="https://github.com/Farama-Foundation/SuperSuit",
+    license_files=("LICENSE.txt",),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["Reinforcement Learning", "game", "RL", "AI", "gymnasium"],
+    python_requires=">=3.7, <3.11",
+    packages=["pettingzoo"]
+    + ["pettingzoo." + pkg for pkg in find_packages("pettingzoo")],
+    include_package_data=True,
+    install_requires=["numpy>=1.18.0", "gymnasium>=0.26.0"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    extras_require=extras,
+
     name="SuperSuit",
     version=get_version(),
     author="SuperSuit Community",
@@ -26,7 +68,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/PettingZoo-Team/SuperSuit",
     keywords=["Reinforcement Learning", "gymnasium"],
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     python_requires=">=3.7",
     install_requires=[
         "pettingzoo>=1.22.0",
