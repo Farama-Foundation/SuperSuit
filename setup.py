6871c22cba8b2be50b3f316b4840d3f4d4e30201
snow-fox <taijunjet@hotmail.com>
2023-01-04 20:59:58 +0000

    setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,60 +1,22 @@
-"""Setups up the SuperSuit module."""
+"""Setups the project."""
 
-from setuptools import find_packages, setup
+import pathlib
+
+from setuptools import setup
 
 
-def get_description():
-    """Gets the description from the readme."""
-    with open("README.md") as fh:
-        long_description = ""
-        header_count = 0
-        for line in fh:
-            if line.startswith("##"):
-                header_count += 1
-            if header_count < 2:
-                long_description += line
-            else:
-                break
-    return header_count, long_description
+CWD = pathlib.Path(__file__).absolute().parent
 
 
 def get_version():
-    """Gets the pettingzoo version."""
-    path = "supersuit/__init__.py"
-    with open(path) as file:
-        lines = file.readlines()
+    """Gets the supersuit version."""
+    path = CWD / "supersuit" / "__init__.py"
+    content = path.read_text()
 
-    for line in lines:
+    for line in content.splitlines():
         if line.startswith("__version__"):
             return line.strip().split()[-1].strip().strip('"')
     raise RuntimeError("bad version data in __init__.py")
 
-version = get_version()
-header_count, long_description = get_description()
 
-setup(
-    name="SuperSuit",
-    version=version,
-    author="Farama Foundation",
-    author_email="contact@farama.org",
-    description="Wrappers for Gymnasium and PettingZoo",
-    url="https://github.com/Farama-Foundation/SuperSuit",
-    license_files=("LICENSE.txt",),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    keywords=["Reinforcement Learning", "game", "RL", "AI", "gymnasium"],
-    python_requires=">=3.7, <3.12",
-    packages=find_packages(),
-    install_requires=["numpy>=1.19.0", "gymnasium>=0.26.0"],
-    extras={"dev": ["pettingzoo[butterfly]"]},
-    classifiers=[
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    include_package_data=True,
-)
+setup(name="supersuit", version=get_version())
