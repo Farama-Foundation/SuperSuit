98f3edf23a38c9cb9b057950e35b4b2f81633fcd
Elliot Tower <elliot@elliottower.com>
2023-07-18 00:24:40 -0400

    Convert to pyproject.toml to fix weird extras issue with pytest

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-"""Setups up the SuperSuit module."""
+import pathlib
 
-from setuptools import find_packages, setup
+from setuptools import setup
+
+
+CWD = pathlib.Path(__file__).absolute().parent
+
+
+def get_version():
+    """Gets the supersuit version."""
+    path = CWD / "supersuit" / "__init__.py"
+    content = path.read_text()
+
+    for line in content.splitlines():
+        if line.startswith("__version__"):
+            return line.strip().split()[-1].strip().strip('"')
+    raise RuntimeError("bad version data in __init__.py")
 
 
 def get_description():
@@ -15,46 +29,7 @@ def get_description():
                 long_description += line
             else:
                 break
-    return header_count, long_description
+    return long_description
 
 
-def get_version():
-    """Gets the pettingzoo version."""
-    path = "supersuit/__init__.py"
-    with open(path) as file:
-        lines = file.readlines()
-
-    for line in lines:
-        if line.startswith("__version__"):
-            return line.strip().split()[-1].strip().strip('"')
-    raise RuntimeError("bad version data in __init__.py")
-
-
-version = get_version()
-header_count, long_description = get_description()
-
-setup(
-    name="SuperSuit",
-    version=version,
-    author="Farama Foundation",
-    author_email="contact@farama.org",
-    description="Wrappers for Gymnasium and PettingZoo",
-    url="https://github.com/Farama-Foundation/SuperSuit",
-    license_files=("LICENSE.txt",),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    keywords=["Reinforcement Learning", "game", "RL", "AI", "gymnasium"],
-    python_requires=">=3.8, <3.12",
-    packages=find_packages(),
-    install_requires=["numpy>=1.19.0", "gymnasium>=0.28.1", "tinyscaler>=1.2.6"],
-    extras={"dev": ["pettingzoo[butterfly,classic]>=1.23.0", "pytest"]},
-    classifiers=[
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    include_package_data=True,
-)
+setup(name="supersuit", version=get_version(), long_description=get_description())
