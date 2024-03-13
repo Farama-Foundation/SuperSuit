345ced200f7731d5534b59a60d50dbdb41887730
elliottower <elliot@elliottower.com>
2024-03-13 14:12:18 -0400

    Pre-commit and rename files

--- test/pettingzoo_env_test.py
+++ test/pettingzoo_env_test.py
@@ -1,6 +1,8 @@
-import pytest
-from pettingzoo.test import parallel_api_test
 from pettingzoo.mpe import simple_spread_v3
+from pettingzoo.test import parallel_api_test
+
+from supersuit.multiagent_wrappers import pad_action_space_v0
+
 
 def test_pad_actuon_space():
     env = simple_spread_v3.parallel_env(max_cycles=25, continuous_actions=True)
